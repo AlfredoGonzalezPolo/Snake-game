303b9fdda862f10a53f128b61d6615949ebe53e3
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-10-21 20:20:27 +0200

    First snakeBlock painted

--- src/Snakegame.java
+++ src/Snakegame.java
@@ -10,20 +10,38 @@ public class Snakegame extends JPanel {
  private class Tile {
   int x;
   int y;
-  Tile(int x, int y){
+
+    Tile(int x, int y){
     this.x = x;
     this.y = y;
   }
  }
  
- 
   int boardWidth;
   int boardHeight;
   int tileSize = 25;
+
+  Tile snakeHead;
 
   Snakegame(int boardWidth, int boardHeight) {
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
     setPreferredSize((new Dimension(this.boardWidth, this.boardHeight)));
+    setBackground(Color.black);
+
+    snakeHead = new Tile(5,5);
+  }
+
+
+  public void paintComponent(Graphics g){
+    super.paintComponent(g);
+    draw(g);
+  }
+
+  public void draw(Graphics g){
+
+    //Snake
+    g.setColor(Color.green);
+    g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
   }
 }
